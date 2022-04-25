658826622ace0516483c25db586daf04296db24b
Binla <495083320@qq.com>
2022-04-25 16:13:20 +0800

    Site updated: 2022-04-25 16:13:20

--- js/main.js
+++ js/main.js
@@ -59,6 +59,39 @@ var alphaDust = function () {
         });
     }
 
+    function initBinlaLogo(){
+        var eyeBallLeft = document.getElementById('eye-ball-l');
+        var eyeBallRight = document.getElementById('eye-ball-r');
+        var mouth = document.getElementById('mouth');
+        if(eyeBallLeft && eyeBallRight && mouth){
+            addEventListener('mousemove', function(e) {
+                var e = e || window.event;
+                _setPos(eyeBallLeft,e,3);
+                _setPos(eyeBallRight,e,3);
+                _setPos(mouth,e,2);
+            },false)
+        }
+    }
+
+    function initToolBar(){
+        var backTop = document.querySelector(".back-top");
+        var time = null;
+        window.onscroll = function () {
+            backTop.style.opacity = (document.documentElement.scrollTop <= 240)? document.documentElement.scrollTop/240 : 1;
+            backTop.style.display = backTop.style.opacity == 0? 'none':'flex';
+        }
+        backTop.onclick = function () {
+            var step = document.documentElement.scrollTop;
+            time = window.setInterval(function () {
+                step -= 100;
+                if (step <= 0) {
+                    window.clearInterval(time);
+                }
+                window.scrollTo(0, step);
+            }, 10);
+        }
+    }
+
     function displayArchives() {
         $('.archive-post').css({opacity: 0});
         TweenMax.staggerTo('.archive-post', 0.4, {opacity: 1}, 0.15);
@@ -69,9 +102,31 @@ var alphaDust = function () {
         TweenMax.staggerTo('.tag-cat', 0.6, {opacity: 1}, 0.15);
     }
 
+    function _setPos(item,moveEvent,moveRange){
+        let leftX = moveEvent.clientX - (item.offsetLeft - item.offsetWidth / 2);
+        let leftY = moveEvent.clientY - (item.offsetTop - item.offsetHeight / 2);
+        let leftPointRadius = Math.floor(_radius({ x: leftX, y: leftY }));
+        let pointRadian = Math.atan2(leftX, leftY);
+        let itemXY= _posXY(moveRange, pointRadian);
+        item.style.cssText = `left:${itemXY.x}px; top: ${itemXY.y}px;`;
+    }
+
+    function _posXY(radius, radian) {
+        return {
+            x: Math.sin(radian) * radius,
+            y: Math.cos(radian) * radius,
+        }
+    }
+
+    function _radius(pos) {
+        return Math.sqrt(Math.pow(pos.x, 2) + Math.pow(pos.y, 2));
+    }
+
     return {
         initPostHeader: initPostHeader,
         initMenu: initMenu,
+        initBinlaLogo: initBinlaLogo,
+        initToolBar:initToolBar,
         displayArchives: displayArchives,
         displayTagAndCat: displayTagAndCat
     };
@@ -81,6 +136,8 @@ var alphaDust = function () {
 $(document).ready(function () {
     alphaDust.initPostHeader();
     alphaDust.initMenu();
+    alphaDust.initBinlaLogo();
+    alphaDust.initToolBar();
     alphaDust.displayArchives();
     alphaDust.displayTagAndCat();
 });
